ed7aa65702a1b9e72d6b95e27344c4a4275573fe
thayanne204 <oliveirathayanne438@gmail.com>
2026-06-08 16:59:44 -0300

    update api url para producao

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api'
+  baseURL: 'https://academia-system-production-3751.up.railway.app/api'
 })
 
 api.interceptors.request.use((config) => {
